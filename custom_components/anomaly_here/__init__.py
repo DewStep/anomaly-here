e257f333430cc1b13210f30e36f42680fc49de63
DewStep <aidan.shaw.uk@gmail.com>
2026-08-14 13:00:00 +0000

    meson-python to requirements

--- custom_components/anomaly_here/__init__.py
+++ custom_components/anomaly_here/__init__.py
@@ -167,8 +167,8 @@ class AnomalyDetector:
             int(current_date[:4]),
             int(current_date[5:7]),
             int(current_date[8:]),
-            13,
-            55,
+            14,
+            10,
             00,
             tzinfo=datetime.UTC,
         )
