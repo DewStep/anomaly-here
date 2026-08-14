85c5427e68e8c1daf8f21703cce3997d03681c39
DewStep <aidan.shaw.uk@gmail.com>
2026-08-14 12:00:16 +0000

    Removes ruptures from manifest for now

--- custom_components/anomaly_here/__init__.py
+++ custom_components/anomaly_here/__init__.py
@@ -167,8 +167,8 @@ class AnomalyDetector:
             int(current_date[:4]),
             int(current_date[5:7]),
             int(current_date[8:]),
-            17,
-            15,
+            13,
+            10,
             00,
             tzinfo=datetime.UTC,
         )
