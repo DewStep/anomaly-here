21accb2b18de814356c7b68b5928906fa6f94b80
DewStep <aidan.shaw.uk@gmail.com>
2026-08-18 13:04:10 +0000

    Tests if episodes made correctly

--- custom_components/anomaly_here/__init__.py
+++ custom_components/anomaly_here/__init__.py
@@ -167,8 +167,8 @@ class AnomalyDetector:
             int(current_date[:4]),
             int(current_date[5:7]),
             int(current_date[8:]),
-            16,
-            00,
+            14,
+            10,
             00,
             tzinfo=datetime.UTC,
         )
