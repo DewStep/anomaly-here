6415524b1718ce4024dd39122b38579bf0911698
DewStep <aidan.shaw.uk@gmail.com>
2026-08-26 11:31:04 +0000

    Changes start time to test

--- custom_components/anomaly_here/__init__.py
+++ custom_components/anomaly_here/__init__.py
@@ -198,7 +198,7 @@ class AnomalyDetector:
             int(current_date[5:7]),
             int(current_date[8:]),
             11,
-            30,
+            40,
             00,
             tzinfo=datetime.UTC,
         )
